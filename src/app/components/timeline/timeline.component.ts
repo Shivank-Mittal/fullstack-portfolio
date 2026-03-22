45dea18b5bedeb3d379b2afda2d79ae4cfb1ac42
shivank-mittal <mittal.shivank@gmail.com>
2026-03-22 17:51:38 +0100

    vercel and ssr settings

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, effect, ElementRef, OnDestroy, OnInit, viewChild } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-timeline',
@@ -7,73 +9,81 @@ import { Component, effect, ElementRef, OnDestroy, OnInit, viewChild } from '@an
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.css',
 })
-export class TimelineComponent{
-
+export class TimelineComponent implements OnInit, OnDestroy {
   experiences = [
-    { title: 'Software Engineer',
+    {
+      title: 'Software Engineer',
       company: 'Tech Company',
       duration: 'Jan 2020 - Present',
-      description: 'Working on developing and maintaining web applications using Angular and Node.js.'
-    },{
+      description:
+        'Working on developing and maintaining web applications using Angular and Node.js.',
+    },
+    {
       title: 'Frontend Developer',
       company: 'Creative Agency',
       duration: 'Jun 2018 - Dec 2019',
-      description: 'Focused on building responsive and user-friendly interfaces using HTML, CSS, and JavaScript.'
-    },{
+      description:
+        'Focused on building responsive and user-friendly interfaces using HTML, CSS, and JavaScript.',
+    },
+    {
       title: 'Intern',
       company: 'Startup Inc.',
       duration: 'Jan 2018 - May 2018',
-      description: 'Assisted in the development of a mobile app using React Native and contributed to UI/UX design.'
-    },{
+      description:
+        'Assisted in the development of a mobile app using React Native and contributed to UI/UX design.',
+    },
+    {
       title: 'Freelance Web Developer',
       company: 'Self-employed',
       duration: '2016 - 2017',
-      description: 'Worked on various freelance projects, creating websites and web applications for small businesses and individuals.'
-    }, 
-    {      title: 'Computer Science Student',
+      description:
+        'Worked on various freelance projects, creating websites and web applications for small businesses and individuals.',
+    },
+    {
+      title: 'Computer Science Student',
       company: 'University of Technology',
       duration: '2014 - 2018',
-      description: 'Studied computer science, focusing on software development, algorithms, and data structures. Participated in coding competitions and contributed to open-source projects.'
-    }
-  ]
+      description:
+        'Studied computer science, focusing on software development, algorithms, and data structures. Participated in coding competitions and contributed to open-source projects.',
+    },
+  ];
 
-  timeline = viewChild<ElementRef>('timeline')
+  timeline = viewChild<ElementRef>('timeline');
   timelineProgress = 0;
+  private isBrowser: boolean;
 
-
-  constructor() {
-    const offset = 500;
-      effect(()=> {
-        const timelineEl = this.timeline();
-        if(!timelineEl) return;
-
-        const element = timelineEl.nativeElement as HTMLElement;
-        window!.addEventListener('scroll', () => {
-          const rect = element.getBoundingClientRect();
-          const windowHeight = window.innerHeight;
-          const timelineTop = rect.top;
-          const timelineHeight = rect.height;
-
-          // how much of timeline is visible
-          const visible = windowHeight - timelineTop - offset;
-
-          // convert to percentage
-          const progress = (visible / timelineHeight) * 100;
-
-          // clamp between 0 and 100
-          this.timelineProgress = Math.min(Math.max(progress, 0), 100);
-        })
-      })
-  }
-  
-  ngAfterViewInit() {
-    const timelineEl = this.timeline();
-    if(!timelineEl) return;
-
-    const element = timelineEl.nativeElement as HTMLElement;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
-  
-  
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      const offset = 500;
+      const timelineEl = this.timeline();
+      if (!timelineEl) return;
 
+      const element = timelineEl.nativeElement as HTMLElement;
+      window.addEventListener('scroll', () => {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        const timelineTop = rect.top;
+        const timelineHeight = rect.height;
+
+        // how much of timeline is visible
+        const visible = windowHeight - timelineTop - offset;
+
+        // convert to percentage
+        const progress = (visible / timelineHeight) * 100;
+
+        // clamp between 0 and 100
+        this.timelineProgress = Math.min(Math.max(progress, 0), 100);
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.isBrowser) {
+      window.removeEventListener('scroll', () => {});
+    }
+  }
 }
--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { BUTTON } from '../../types/TButtons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBrain, faBriefcase, faGaugeHigh, faGuaraniSign, faProjectDiagram, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-hero',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, FontAwesomeModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
 export class HeroComponent {
   
   getInTouchButton = BUTTON.OUTLINE
+
+  icons = {
+    briefcase:  faBriefcase,
+    projectDiagram: faBrain,
+    user: faGaugeHigh
+  }
 
 }
